b3578bb532b1b8ad2a8f0e85d8ed7985a3898da1
kwj <1593359878@qq.com>
2018-10-23 16:12:31 +0800

    更新1

---
+++ 冒险岛-静态/js/index.js
@@ -0,0 +1,11 @@
+// window.onload function(){
+	// alert('123');
+	function banben(){
+		let banben_arr = ['进击！魔方之眼','七夕鹊桥来相会','觉醒公测 海量更新','暑期版本大爆料','快乐端午粽香四溢',
+		'苏醒的蘑菇王','五一欢庆版','春季总动员vol.3','春季总动员vol.2','春季总动员vol.1','新冒险·新春曲','新冒险·迎新曲','新冒险·圣诞曲','真心话·新冒险','新冒险·续章'];
+		for(let i = 0 ; i < banben_arr.length ; i++){
+			document.write('<li><a href="">'+banben_arr[i]+'</a></li>');
+			// console.log(banben_arr[i]);
+		}
+	}
+// }
